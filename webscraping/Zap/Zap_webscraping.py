8ec3b731406972110c0c848f279d51f48ca87244
João Frugis <joaofrugis@gmail.com>
2023-11-06 20:07:39 -0300

    Zap with link

--- webscraping/Zap/Zap_webscraping.py
+++ webscraping/Zap/Zap_webscraping.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import googlemaps
 from urllib.request import urlopen, Request
 from bs4 import BeautifulSoup
 from unidecode import unidecode
@@ -16,7 +17,9 @@ pages = 2
 
 chrome_options = Options()
 chrome_options.add_argument("--start-maximized")
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
+gmaps = googlemaps.Client(key='AIzaSyArMwSvnOgwWi68S80guZDdk9L5nHztvhQ')
+
 for i in range(1,pages+1):
     driver = webdriver.Chrome(options=chrome_options)
 
@@ -24,7 +27,7 @@ for i in range(1,pages+1):
 
     time.sleep(3)
     for _ in range(68):
-        time.sleep(0.5)
+        time.sleep(0.8)
         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
         time.sleep(1)
 
@@ -51,6 +54,11 @@ for i in range(1,pages+1):
                 headers={'User-Agent': 'Mozilla/5.0'}
             )
 
+            try: 
+                html_content = urlopen(request).read().decode('utf-8')    
+            except:
+                continue
+
             html_content = urlopen(request).read().decode('utf-8')
             content = BeautifulSoup(html_content, 'html.parser')
             
@@ -59,6 +67,10 @@ for i in range(1,pages+1):
             card['tipo'] = content.find('span', {"class": "info__business-type color-dark text-regular"}).get_text().split('para')[0].strip()
 
             card['endereco'] = content.find('span', {"class": "link"}).get_text().strip()
+
+            result = gmaps.geocode(card['endereco'] + ',Leme,SP')
+            card['lat'] = result[0]['geometry']['location']['lat']
+            card['long'] = result[0]['geometry']['location']['lng']
 
             card['dormitorios'] = 0
             if content.find('span', {"itemprop": "numberOfRooms"}):
@@ -77,6 +89,8 @@ for i in range(1,pages+1):
                 card['area'] = content.find('span', {"itemprop": "floorSize"}).get_text().strip().split(' ')[0]
             
             card['valor'] = content.find('li', {'class': 'price__item--main text-regular text-regular__bolder'}).find('strong').get_text().split('R$')[1].strip().replace('.','').replace(',','.')
+
+            card['url_anuncio'] = url_anuncio
             cards.append(card)
             count += 1
         else:
@@ -102,6 +116,7 @@ for i in range(1,pages+1):
             links = soup_anuncio_multiplo.findAll('a', {'class': 'deduplication-card__anchor'})
             valores_imoveis = []
             for link in links:
+                url_anuncio = link['href']
                 id_card = link['data-id']
                 valores_imoveis.append({id_card: float(link.find('div' , {"class", "listing-price"}).find('p').get_text().split(' ')[1].replace('.','').replace(',','.'))})
 
@@ -156,9 +171,15 @@ for i in range(1,pages+1):
                 card['area'] = content.find('span', {"itemprop": "floorSize"}).get_text().strip().split(' ')[0]
             
             card['valor'] = content.find('li', {'class': 'price__item--main text-regular text-regular__bolder'}).find('strong').get_text().split('R$')[1].strip().replace('.','')
+
+            card['url_anuncio'] = url_anuncio
+            
             cards.append(card)
             count += 1
-
-
+        
+        df = pd.DataFrame(cards)    
+        df.to_csv('./Zap_data.csv', ';',index=False)
+    
+        
 df = pd.DataFrame(cards)
-df.to_csv('./data.csv', ';')
+df.to_csv('./Zap_data.csv', ';',index=False)
